somhi/jtframe

commit fe9379510261171fc43d37df7d036d438cdc3ff7

Force 60Hz option

target/mister/jtframe_mister.sv
@@ -272,11 +272,11 @@ assign vcopt      = status[45:42];
 assign crop_scale = {1'b0, status[47:46]};
 
 // H-Pos & V-Pos for CRT
-assign { voffset, hoffset } = status[31:24];
+assign { voffset, hoffset } = status[60:53];
 
 // Horizontal scaling for CRT
-assign hsize_enable = status[19];
-assign hsize_scale  = status[23:20];
+assign hsize_enable = status[48];
+assign hsize_scale  = status[52:49];
 
 `ifdef JTFRAME_VERTICAL
 assign {FB_PAL_CLK, FB_FORCE_BLANK, FB_PAL_ADDR, FB_PAL_DOUT, FB_PAL_WR} = '0;
@@ -311,8 +311,10 @@ jtframe_resync u_resync(
     .vs_out     ( vs_resync     )
 );
 
-wire [15:0] status_menumask; // a high value hides the menu item
 reg         framebuf_flip;      // extra OSD options for rotation, bit 0 = rotate, bit 1 = flip
+
+// OSD option visibility
+wire [15:0] status_menumask; // a high value hides the menu item
 
 assign status_menumask[15:6] = 0,
        status_menumask[5]    = crop_ok, // video crop options
@@ -322,6 +324,9 @@ assign status_menumask[15:6] = 0,
 `else
        status_menumask[4]    = 1,   // hidden
        status_menumask[1]    = ~core_mod[0],  // shown for vertical games
+`endif
+`ifdef JTFRAME_OSD60HZ
+       status_menumask[3]    = status[19],      // Disables the Scan FX options if a core needs the 60Hz option but is not set
 `endif
        status_menumask[2]    = ~hsize_enable,    // horizontal scaling
        status_menumask[0]    = direct_video;

hdl/jtframe_board.v
@@ -793,7 +793,6 @@ function [7:0] extend8;
 endfunction
 
 `ifndef MISTER
-
     // This scan doubler takes very little memory. Some games in MiST
     // can only use this
     wire [CLROUTW*3-1:0] rgbx2;

hdl/jtframe_dip.v
@@ -99,6 +99,9 @@ wire [1:0] ar = status[17:16];    // only MiSTer
 `ifdef MISTER
 always @(*) begin
     scanlines = status[5:3];
+`ifdef JTFRAME_OSD60HZ
+    if ( !status[19] ) scanlines=0;
+`endif
     bw_en     = 0;      // Old TV filter disabled in MiSTer, is not needed anymore
     blend_en  = 0;
 end

target/mist/cfgstr
@@ -1,6 +1,7 @@
 {{.CORENAME}};;
 
 {{ if not .JTFRAME_OSD_NOLOAD }}    F,rom; {{ end }}
+{{ if .JTFRAME_OSD60HZ }}           OJ,Force 60Hz, Off, On; {{ end }}
 {{ if .JTFRAME_VERTICAL }}
 {{ if .JTFRAME_OSD_FLIP }}          O1,Flip screen,Off,On; {{ end }}
                                     O2,Rotate controls,No,Yes; {{ end }}

target/mister/cfgstr
@@ -8,14 +8,15 @@ J{{.CORENAME}};;
 {{ if .JTFRAME_CHEAT }}
     CHEAT;
 {{ end }}
-P1,Video;
-P1OOR,CRT H offset,0,+1,+2,+3,+4,+5,+6,+7,-8,-7,-6,-5,-4,-3,-2,-1;
-P1OSV,CRT V offset,0,+1,+2,+3,+4,+5,+6,+7,-8,-7,-6,-5,-4,-3,-2,-1;
-P1OJ,CRT scale enable,Off,On;
-H2P1OKN,CRT scale factor,0,+1,+2,+3,+4,+5,+6,+7,-8,-7,-6,-5,-4,-3,-2,-1;
+    P1,Video;
+{{ if .JTFRAME_OSD60HZ }}           P1OJ,Force 60Hz, Off, On; {{ end }}
+    P1oLN,CRT H offset,0,+1,+2,+3,+4,+5,+6,+7,-8,-7,-6,-5,-4,-3,-2,-1;
+    P1oPS,CRT V offset,0,+1,+2,+3,+4,+5,+6,+7,-8,-7,-6,-5,-4,-3,-2,-1;
+    P1oG,CRT scale enable,Off,On;
+    H2P1oHK,CRT scale factor,0,+1,+2,+3,+4,+5,+6,+7,-8,-7,-6,-5,-4,-3,-2,-1;
 P1{{ .SEPARATOR }}
-{{ if .JTFRAME_NOHQ2X }}            P1O35,Scandoubler Fx,None,N/A,CRT 25%,CRT 50%,CRT 75%;
-        {{ else }}                  P1O35,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%; {{ end }}
+{{ if .JTFRAME_NOHQ2X }}            d3P1O35,Scandoubler Fx,None,N/A,CRT 25%,CRT 50%,CRT 75%;
+        {{ else }}                  d3P1O35,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%; {{ end }}
 H0P1OGH,Aspect ratio,Original,Full screen,[ARC1],[ARC2];
 {{ if .JTFRAME_VERTICAL }}
     {{ if .JTFRAME_ROTATE }}        H4P1o78,Rotate screen,Yes,No (original),No (flip);

commit 62867890ae43d5fe51ea2dab8b565b1f0bd18cdd

Fixes FX options not available when JTFRAME_OSD60HZ is not defined. Bug introduced in fe93795

target/mister/jtframe_mister.sv
@@ -327,6 +327,8 @@ assign status_menumask[15:6] = 0,
 `endif
 `ifdef JTFRAME_OSD60HZ
        status_menumask[3]    = status[19],      // Disables the Scan FX options if a core needs the 60Hz option but is not set
+    `else
+       status_menumask[3]    = 1,
 `endif
        status_menumask[2]    = ~hsize_enable,    // horizontal scaling
        status_menumask[0]    = direct_video;