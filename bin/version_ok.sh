2454761c471de51202ec26c1bb202d3d1825ef9c
Jose Tejada <jotego@gmail.com>
2023-01-12 08:17:52 +0100

    beta scripts

--- bin/version_ok.sh
+++ bin/version_ok.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+# This file is part of JTFRAME.
+# JTFRAME program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# JTFRAME program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with JTFRAME.  If not, see <http://www.gnu.org/licenses/>.
+
+# Author: Jose Tejada Gomez. Twitter: @topapate
+# Version: 1.0
+# Date: 12-1-2023
 
 PRJCOMMIT=$(git rev-parse --short HEAD)
 
 # if there is a version tag that matches the commit, use it instead
 PRJTAG=`git tag --contains $PRJCOMMIT | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+$" | tail -n 1`
 if [ ! -z "$PRJTAG" ]; then
-    PRJCOMMIT=$PRJTAG;
-    if [ "$1" = -v ]; then echo "Using version tag $PRJCOMMIT"; fi
+    echo $PRJTAG;
     exit 0
 else
-    echo "Error: cannot use --git if there is no version tag for the commit"
-    echo "Create the first version tag manually. Use jtmerge to create the tags after that."
+    cat > /dev/stderr <<EOF
+Error: cannot use --git if there is no version tag for the commit
+Create the first version tag manually. Use jtmerge to create the tags after that.
+EOF
     exit 1
 fi
