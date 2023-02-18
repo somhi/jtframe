2da4ce7c344f63a25d5e505535a6776df3a27fa9
Jose Tejada <jotego@gmail.com>
2023-02-18 21:58:55 +0100

    jtcore17.df doesn't work in GitHub. Why?

--- devops/build-dockers.sh
+++ devops/build-dockers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker image build --file jtcore-base.df --tag jotego/jtcore-base .
 docker image build --file jtcore13.df --tag jotego/jtcore13 /opt/altera
+docker image build --file jtcore17.df --tag jotego/jtcore17 /opt/intelFPGA_lite
 docker login
 docker push jotego/jtcore-base:latest
 docker push jotego/jtcore13:latest
+docker push jotego/jtcore17:latest
