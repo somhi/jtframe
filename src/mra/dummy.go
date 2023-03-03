3f54f652ae7aed0135d0607b0bdfb83eaf607558
Jose Tejada <jotego@gmail.com>
2023-03-03 07:59:48 +0100

    Fixes dummy pocket.go

--- src/mra/dummy.go
+++ src/mra/dummy.go
@@ -6,7 +6,7 @@ import "fmt"
 
 var pocket_warning bool
 
-func pocket_add(machine *MachineXML, cfg Mame2MRA, args Args, def_dipsw string) {
+func pocket_add(machine *MachineXML, cfg Mame2MRA, args Args, def_dipsw string, coremod int) {
 	if args.Verbose && !pocket_warning {
 		fmt.Println("****  Skipping Pocket file generation ****")
 		pocket_warning = true
